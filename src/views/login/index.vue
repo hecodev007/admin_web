1097f7fcdc1afdc1e4faadb9e739d089875d955e
jaaia <go787888@gmail.com>
2025-01-08 15:46:34 +0800

    feat: login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -11,28 +11,45 @@
       img(src="../../assets/wlogo.png")
       .text {{$lang('欢迎登录')}}
       p {{$lang('SaaS管理平台')}}
-    Form(ref="formValidate", :model="formItem", :rules="ruleValidate")
+    Form(ref="formValidate", :model="formItem", :rules="ruleValidate" v-if="step === 0")
       FormItem(:label="$lang('账号')" prop="username")
         Input(@on-enter="submit" v-model="formItem.username" prefix="md-person" , :placeholder="$lang('请输入',$lang('账号'))")
       FormItem(:label="$lang('密码')" prop="password")
         Input(@on-enter="submit" v-model="formItem.password" type="password" prefix="md-lock", :placeholder="$lang('请输入',$lang('密码'))")
       FormItem
-        Button(type="primary" @click="submit" long ghost icon="md-send", :loading="loading") {{$lang('登录')}}
-    a(href="/password/find")  {{$lang('忘记密码')}} ？
+        Button(type="primary" @click="login" long ghost icon="md-send", :loading="loading") {{$lang('登录')}}
+    Form(ref="formGoogleCodeValidate", :model="formGoogleCode", :rules="ruleValidate" v-if="step === 1")
+      FormItem(:label="$lang('Google验证码')" prop="formGoogleCode")
+        Input(@on-enter="submit" v-model="formGoogleCode.code" prefix="md-person" , :placeholder="$lang('请输入',$lang('Google验证码'))")
+      FormItem
+        img(:src="qrCode" width="200" height="200")
+      FormItem
+        Button(type="primary" @click="bind" long ghost icon="md-send", :loading="loading") {{$lang('登录')}}
+    Form(ref="formGoogleCodeValidate", :model="formGoogleCode", :rules="ruleValidate" v-if="step === 2")
+      FormItem(:label="$lang('Google验证码')" prop="formGoogleCode")
+        Input(@on-enter="submit" v-model="formGoogleCode.code" prefix="md-person" , :placeholder="$lang('请输入',$lang('Google验证码'))")
+      FormItem
+        Button(type="primary" @click="verify" long ghost icon="md-send", :loading="loading") {{$lang('登录')}}
+    div(v-if="step === 0")
+      a(href="/password/find")  {{$lang('忘记密码')}} ？
 </template>
 <script>
-import md5 from "md5";
 // function
 import { refreshRoute } from "@/router"; // 路由初始化，清空动态路由
-import { localRead, localSave } from "@/libs/util";
+import { localSave } from "@/libs/util";
 
 export default {
   data() {
     return {
+      step: 0,
       loading: false,
+      qrCode: "",
       formItem: {
         username: "",
         password: ""
+      },
+      formGoogleCode: {
+        code: ""
       },
       ruleValidate: {
         username: [
@@ -76,7 +93,7 @@ export default {
     setLocale(lang) {
       this.$store.commit("setLocale", lang);
     },
-    submit() {
+    login() {
       this.$refs["formValidate"].validate(valid => {
         if (valid) {
           this.loading = true;
@@ -85,20 +102,65 @@ export default {
           this.$axios.post("/api/v1/user/login", { username, password }).then(result => {
             this.loading = false;
             if (result.code === 10000) {
-              // 正常跳转
-              let redirectUrl = sessionStorage.redirectUrlDemo;
-              if (redirectUrl) {
-                sessionStorage.removeItem("redirectUrl");
+              if(result.data.googleBind){
+                this.step = 1;
+                this.getImg()
               } else {
-                redirectUrl = "/";
+                this.step = 2;
               }
-              location.href = redirectUrl;
-              this.$store.commit("setUserName", result.data);
-              localSave('userInfo', JSON.stringify(result.data))
             }
           });
         }
       });
+    },
+    verify(){
+      this.$refs["formGoogleCodeValidate"].validate(valid => {
+        if (valid) {
+          this.loading = true;
+          const code = this.formGoogleCode.code;
+          this.$axios.post("/api/v1/user/google_verify", { code }).then(res => {
+            this.loading = false;
+            if (res.code === 10000) {
+              this.jump(res)
+            } else if (res.code === 10001) {
+              this.$Message.error(this.$lang("Google验证码错误"));
+            }
+          });
+        }
+      });
+    },
+    bind(){
+      this.$refs["formGoogleCodeValidate"].validate(valid => {
+        if (valid) {
+          this.loading = true;
+          const code = this.formGoogleCode.code;
+          this.$axios.post("/api/v1/user/google_code_bind", { code }).then(res => {
+            if (res.code === 10000) {
+              this.jump(res)
+            } else if (res.code === 10001) {
+              this.$Message.error(this.$lang("Google验证码错误"));
+            }
+          });
+        }
+      });
+    },
+    getImg(){
+      this.$axios.get("/api/v1/user/get_google_code").then(res => {
+        if (res.code === 10000) {
+          this.qrCode = res.data.secret_url;
+        }
+      });
+    },
+    jump(result){
+      let redirectUrl = sessionStorage.redirectUrlDemo;
+      if (redirectUrl) {
+        sessionStorage.removeItem("redirectUrl");
+      } else {
+        redirectUrl = "/";
+      }
+      location.href = redirectUrl;
+      this.$store.commit("setUserName", result.data);
+      localSave('userInfo', JSON.stringify(result.data))
     }
   }
 };
